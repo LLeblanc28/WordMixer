5c99a97df712350d6c75253949e1dd438d75e233
leo LEBLANC <leoleblanc28@gmail.com>
2026-02-06 10:59:23 +0100

    test_wordmixer

--- test_wordmixer.py
+++ test_wordmixer.py
@@ -1,24 +1,35 @@
 import pytest
-from my_project import create_app
-
-@pytest.fixture()
-def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
-
-    # other setup can go here
-
-    yield app
-
-    # clean up / reset resources here
+from wordmixer import mix_word
 
 
-@pytest.fixture()
-def client(app):
-    return app.test_client()
+def test_mix_word():
+    word = 'aaaa'
+    actual = mix_word(word)
+    assert len(word) == len(actual)
+    for letter in word:
+        assert letter in actual
 
-def test_request_example(client):
-    response = client.get("/posts")
-    assert b"<h2>Hello, World!</h2>" in response.data
+
+
+# @pytest.fixture()
+# def app():
+#     app = create_app()
+#     app.config.update({
+#         "TESTING": True,
+#     })
+
+#     # other setup can go here
+
+#     yield app
+
+#     # clean up / reset resources here
+
+
+# @pytest.fixture()
+# def client(app):
+#     return app.test_client()
+
+# def test_request_example(client):
+#     response = client.get("/posts")
+#     assert b"<h2>Hello, World!</h2>" in response.data
+
--- wordmixer.py
+++ wordmixer.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def mix_word(word):
+    return word
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/mix')
 def result():
-    wordmixed = request.args.get('word', '')
+    word = request.args.get('word', '')
+    wordmixed = mix_word(word)
     return wordmixed
